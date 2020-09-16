7812392bb29bf655bab35ebbc55a5fedb727dd5c
Manish Ranjan Behera <manish12042000@gmail.com>
2020-09-16 20:40:28 +0530

    Fibonacci

---
+++ nth Fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int fibonacci(int n){
+  if(n<=1)
+  {
+   return n;
+  }
+  else
+  {
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+  
+}
+
+int main()
+{
+  int n;
+  printf("Enter a Number:");
+  scanf("%d",&n);
+  printf("The nth term of the Fibanacci Series is %d",fibonacci(n));
+}
