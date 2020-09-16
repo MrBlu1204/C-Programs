2aa32bfff18770b1fdfc89b880a99865520560e9
Manish Ranjan Behera <manish12042000@gmail.com>
2020-09-16 20:47:42 +0530

    Correction

--- nthFibonacci.c
+++ nthFibonacci.c
@@ -2,15 +2,10 @@
 #include<stdlib.h>
 
 int fibonacci(int n){
-  if(n==0)
+  if(n<=1)
   {
-   return 0;
+   return n;
   }
-  else if (n==1)
-  {
-    return 1;
-  }
-  
   else
   {
     return fibonacci(n-1) + fibonacci(n-2);
@@ -23,6 +18,6 @@ int main()
   int n;
   printf("Enter a Number:");
   scanf("%d",&n);
-  int result =fibonacci(n);
+  int result =fibonacci(n-1);
   printf("The nth term of the Fibanacci Series is %d",result);
 }
